d9ce56ebf10cb562823f9c1579c320956c5dbdec
Randy.Vargas96 <randyvargas@pursuit.org>
2023-06-08 14:38:04 -0400

    first commit

---
+++ index .js
@@ -0,0 +1,27 @@
+const importWords = require("./words.json");
+
+console.log(importWords);
+//logs all words
+
+function allWords() {}
+
+// logs the first ten
+function firstTenWords() {}
+
+//log next ten
+function nextTenWords() {}
+
+//logs from 0 to x words
+function firstXWords(){}
+
+// logs x to y words
+function subsetOfWords(){}
+
+
+//logs all words, sort by alphabet.
+function sortWords(){}
+
+// only words with Q
+function wordsdWithQ(){}
+
+function findWordsWithLetter(A){}
